d98061d2bd2cb7b79a49179fdd558ade78a59638
shafeeq <shafeeqpem@gmail.com>
2019-03-07 07:27:47 -0800

    height Chnged

--- src/components/HomeHeader.js
+++ src/components/HomeHeader.js
@@ -8,7 +8,7 @@ class HomeHeader extends Component {
     return (
       <div className="HomeHeader">
         <header className="home-header">
-          <h1 className="App-logo">XPERTCUBE REACT WEBSITE</h1>
+          <h1 className="App-logo">Test</h1>
           <h2 className="App-header-title">HOME</h2>
         </header>
       </div>
